b85071fb212797d60bfae4aa46b3429189077fcb
Huy Vu <huyvu@Huys-MacBook-Pro.local>
2025-10-08 14:32:04 +0700

    hide

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
 import express from "express";
 import cors from "cors";
 
-import userRouter from "./routes/userRoutes.js"
-import taskRouter from "./routes/taskRoutes.js"
+import userRouter from "./routes/userRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
 import activityRouter from "./routes/activityRoutes.js";
-
 import { authenticateToken } from "./middleware/auth.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
 
-app.use("/api/users", userRouter)
-app.use("/api/tasks", authenticateToken, taskRouter)
-app.use("/api/activities", authenticateToken, activityRouter)
+app.use(
+  cors({
+    origin: process.env.UI_APP_ENDPOINT || "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  })
+);
+
+app.options("*", cors());
+
+app.use("/api/users", userRouter);
+app.use("/api/tasks", authenticateToken, taskRouter);
+app.use("/api/activities", authenticateToken, activityRouter);
 
 export default app;
-
